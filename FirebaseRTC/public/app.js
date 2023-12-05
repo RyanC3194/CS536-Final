bcfa257313a558ca2f61622d35a54aacff037305
nathanhyao <nathanhyao@gmail.com>
2023-12-04 20:46:33 -0500

    choose media device

--- FirebaseRTC/public/app.js
+++ FirebaseRTC/public/app.js
@@ -1,6 +1,6 @@
 mdc.ripple.MDCRipple.attachTo(document.querySelector('.mdc-button'));
 
-const configuration = {
+let configuration = {
   iceServers: [
     {
       urls: [
@@ -18,6 +18,12 @@ let remoteStream = null;
 let roomDialog = null;
 let roomId = null;
 
+// Replace this once the OpenRelay TURN server's 50GB quota usage has been reached.
+const TURN_API_KEY = "https://nhyao.metered.live/api/v1/turn/credentials?apiKey=28c84515a1faf51ce04566f25404dfa4c3d5";
+
+// IMPORTANT: If testing locally, let useTURN = false; Set to true before deploying on Firebase Hosting.
+let useTURN = true;
+
 function init() {
   document.querySelector('#cameraBtn').addEventListener('click', openUserMedia);
   document.querySelector('#hangupBtn').addEventListener('click', hangUp);
@@ -31,6 +37,13 @@ async function createRoom() {
   document.querySelector('#joinBtn').disabled = true;
   const db = firebase.firestore();
   const roomRef = await db.collection('rooms').doc();
+
+  if (useTURN) {
+    const response = await fetch(TURN_API_KEY);
+    const iceServers = await response.json();
+    console.log(iceServers);
+    configuration = { iceServers: iceServers };
+  }
 
   console.log('Create PeerConnection with configuration: ', configuration);
   peerConnection = new RTCPeerConnection(configuration);
@@ -69,7 +82,7 @@ async function createRoom() {
   roomId = roomRef.id;
   console.log(`New room created with SDP offer. Room ID: ${roomRef.id}`);
   document.querySelector(
-      '#currentRoom').innerText = `Current room is ${roomRef.id} - You are the caller!`;
+    '#currentRoom').innerText = `Current room is ${roomRef.id} - You are the caller!`;
   // Code for creating a room above
 
   peerConnection.addEventListener('track', event => {
@@ -102,6 +115,8 @@ async function createRoom() {
     });
   });
   // Listen for remote ICE candidates above
+
+  localConnection = peerConnection;
 }
 
 function joinRoom() {
@@ -109,13 +124,13 @@ function joinRoom() {
   document.querySelector('#joinBtn').disabled = true;
 
   document.querySelector('#confirmJoinBtn').
-      addEventListener('click', async () => {
-        roomId = document.querySelector('#room-id').value;
-        console.log('Join room: ', roomId);
-        document.querySelector(
-            '#currentRoom').innerText = `Current room is ${roomId} - You are the callee!`;
-        await joinRoomById(roomId);
-      }, {once: true});
+    addEventListener('click', async () => {
+      roomId = document.querySelector('#room-id').value;
+      console.log('Join room: ', roomId);
+      document.querySelector(
+        '#currentRoom').innerText = `Current room is ${roomId} - You are the callee!`;
+      await joinRoomById(roomId);
+    }, { once: true });
   roomDialog.open();
 }
 
@@ -124,6 +139,12 @@ async function joinRoomById(roomId) {
   const roomRef = db.collection('rooms').doc(`${roomId}`);
   const roomSnapshot = await roomRef.get();
   console.log('Got room:', roomSnapshot.exists);
+
+  if (useTURN) {
+    const response = await fetch(TURN_API_KEY);
+    const iceServers = await response.json();
+    configuration = { iceServers: iceServers };
+  }
 
   if (roomSnapshot.exists) {
     console.log('Create PeerConnection with configuration: ', configuration);
@@ -186,7 +207,12 @@ async function joinRoomById(roomId) {
 
 async function openUserMedia(e) {
   const stream = await navigator.mediaDevices.getUserMedia(
-      {video: true, audio: true});
+    { video: true, audio: true });
+
+  // Browser needs permission before displaying media devices, so auto refresh.
+  navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
+  console.log("permissioned granted, media devices enumerated");
+
   document.querySelector('#localVideo').srcObject = stream;
   localStream = stream;
   remoteStream = new MediaStream();
@@ -242,7 +268,7 @@ async function hangUp(e) {
 function registerPeerConnectionListeners() {
   peerConnection.addEventListener('icegatheringstatechange', () => {
     console.log(
-        `ICE gathering state changed: ${peerConnection.iceGatheringState}`);
+      `ICE gathering state changed: ${peerConnection.iceGatheringState}`);
   });
 
   peerConnection.addEventListener('connectionstatechange', () => {
@@ -253,10 +279,145 @@ function registerPeerConnectionListeners() {
     console.log(`Signaling state change: ${peerConnection.signalingState}`);
   });
 
-  peerConnection.addEventListener('iceconnectionstatechange ', () => {
+  peerConnection.addEventListener('iceconnectionstatechange', () => {
     console.log(
-        `ICE connection state change: ${peerConnection.iceConnectionState}`);
+      `ICE connection state change: ${peerConnection.iceConnectionState}`);
   });
 }
 
 init();
+
+/* ########################################################################## */
+
+/* Select sources & outputs. I.e., choose from available camera(s), microphone(s), and speaker(s) */
+const localVideoElement = document.querySelector("#localVideo");
+const audioInputSelect = document.querySelector("select#audioSource");
+const audioOutputSelect = document.querySelector("select#audioOutput");
+const videoSelect = document.querySelector("select#videoSource");
+const selectors = [audioInputSelect, audioOutputSelect, videoSelect];
+
+function gotDevices(deviceInfos) {
+
+  // This will handle being called several times when updating options. Preserve values.
+  const values = selectors.map(select => select.value);
+  selectors.forEach(select => {
+    while (select.firstChild) {
+      select.removeChild(select.firstChild);
+    }
+  });
+
+  for (let i = 0; i < deviceInfos.length; i++) {
+    const deviceInfo = deviceInfos[i];
+    const option = document.createElement("option");
+    option.value = deviceInfo.deviceId;
+
+    if (deviceInfo.kind === "audioinput") {
+      option.text = deviceInfo.label || `microphone ${audioInputSelect.length + 1} (check permissions)`;
+      audioInputSelect.appendChild(option);
+    }
+    else if (deviceInfo.kind === "audiooutput") {
+      option.text = deviceInfo.label || `speaker ${audioOutputSelect.length + 1} (check permissions)`;
+      audioOutputSelect.appendChild(option);
+    }
+    else if (deviceInfo.kind === "videoinput") {
+      option.text = deviceInfo.label || `camera ${videoSelect.length + 1} (check permissions)`;
+      videoSelect.appendChild(option);
+    }
+    else {
+      console.log("Some other kind of source/device: ", deviceInfo);
+    }
+  }
+
+  selectors.forEach((select, selectorIndex) => {
+    // console.log(Array.prototype.slice.call(select.childNodes)); // DEBUG
+    if (Array.prototype.slice.call(select.childNodes).some(n => n.value === values[selectorIndex])) {
+      select.value = values[selectorIndex];
+    }
+  });
+}
+
+function handleError(error) {
+  console.log("navigator.MediaDevices.getUserMedia error: ", error.message, error.name);
+}
+
+/* navigator.mediaDevices.enumerateDevices() will return a promise that resolves to an array of
+  MediaDevicesInfo that describe each known media device. */
+navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
+
+/* Attach audio output device to video element using device/sink ID. */
+function attachSinkId(element, sinkId) {
+  if (typeof element.sinkId === "undefined") {
+    console.warn("Browser does not support output device selection.");
+    return;
+  }
+  element.setSinkId(sinkId)
+    .then(() => {
+      console.log(`Success, audio output device attached: ${sinkId}`);
+    })
+    .catch(error => {
+      let errorMessage = error;
+      if (error.name === "SecurityError") {
+        errorMessage = `You need to use HTTPS for selecting audio output device: ${error}`;
+      }
+      console.error(errorMessage);
+      // Jump back to first output device in the list as it's the default.
+      audioOutputSelect.selectedIndex = 0;
+    });
+}
+
+function changeAudioDestination() {
+  const audioDestination = audioOutputSelect.value;
+  attachSinkId(localVideoElement, audioDestination);
+}
+
+function gotStream(stream) {
+  localStream = stream; // make stream available to console
+  localVideoElement.srcObject = stream;
+  return navigator.mediaDevices.enumerateDevices(); // Refresh list of available devices
+}
+
+function switchTracks() {
+  connections = [peerConnection];
+  const audioSource = audioInputSelect.value;
+  const videoSource = videoSelect.value;
+  navigator.mediaDevices
+    .getUserMedia({
+      audio: { deviceId: audioSource ? { exact: audioSource } : undefined },
+      video: { deviceId: videoSource ? { exact: videoSource } : undefined }
+    })
+    .then((stream) => {
+      const [videoTrack] = stream.getVideoTracks();
+      const [audioTrack] = stream.getAudioTracks();
+      connections.forEach((pc) => {
+        const videoSender = pc
+          .getSenders()
+          .find((s) => s.track.kind === videoTrack.kind);
+        console.log("Found video sender:", videoSender);
+        videoSender.replaceTrack(videoTrack);
+
+        const audioSender = pc
+          .getSenders()
+          .find((s) => s.track.kind === audioTrack.kind);
+        console.log("Found audio sender:", audioSender);
+        audioSender.replaceTrack(audioTrack);
+
+        localStream = stream; // make stream available to console
+        localVideoElement.srcObject = stream;
+      });
+    })
+    .catch((err) => {
+      console.error(`Error happened: ${err}`);
+    });
+
+  // Refresh list of available devices
+  navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
+}
+
+/* The sinkId property is part of the Web Audio API and allows developers to select the output 
+  device for audio. */
+audioOutputSelect.disabled = !("sinkId" in HTMLMediaElement.prototype);
+
+audioInputSelect.onchange = switchTracks;
+audioOutputSelect.onchange = changeAudioDestination;
+
+videoSelect.onchange = switchTracks;
